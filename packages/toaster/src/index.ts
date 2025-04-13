839e1491df2ed55e6711d87ea6e43800b6ab6681
Horatio Wang <wh5938316@gmail.com>
2025-04-13 22:58:23 +0800

    fix: add additional exports for Toaster module

--- packages/toaster/src/index.ts
+++ packages/toaster/src/index.ts
@@ -1,6 +1,7 @@
 export { default as Toast } from './Toast';
 export { default as toaster } from './ToasterEvents';
 export { default as Toaster } from './Toaster';
+export * from './Toaster';
 export * from './Toast';
 export * from './toasterClasses';
 export * from './toastClasses';
